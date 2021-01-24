f1dc0c3a3039d63510cadcab234d152abd4dd15b
priyakhot64 <priyakhot64@gmail.com>
2021-01-24 11:13:39 +0530

    13JRTP-code cleanup

--- App-01/Demo1.java
+++ App-01/Demo1.java
@@ -1,7 +1,5 @@
 public class Demo{
 	public static void main(String[] args){
-	       int a =10;
-	       int b =20;
 	       System.out.println("Hello");
 	}
 }
--- Demo1.java
+++
@@ -1,7 +0,0 @@
-public class Demo{
-	public static void main(String[] args){
-	       int a =10;
-	       int b =20;
-	       System.out.println("Hello");
-	}
-}
